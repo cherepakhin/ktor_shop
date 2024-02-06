289215d60a4f006752bc0fcf09fac09b6c3779a5
Черепахин Василий <vasi.che@gmail.com>
2024-02-06 18:06:34 +0500

    add elvis compare to SimpleNullTest.kt

--- src/test/kotlin/ru/perm/v/ktor/SimpleNullTest.kt
+++ src/test/kotlin/ru/perm/v/ktor/SimpleNullTest.kt
@@ -5,13 +5,25 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class SimpleNullTest {
+    private fun getNullLong(): Long? {
+        return null
+    }
+
     @Test
     fun convertNullLong() {
-        val n = null
+        val n = getNullLong()
 
         assertNull(n)
         assertEquals(-1L, if (n == null) -1 else n)
+    }
+
+    @Test
+    fun convertNullWithElvis() {
+        val n = getNullLong()
+
+        assertNull(n)
         // Elvis operator
-        assertEquals(-1L, n ?: -1L)
+        val nn = n ?: -1L
+        assertEquals(-1L, nn)
     }
 }
